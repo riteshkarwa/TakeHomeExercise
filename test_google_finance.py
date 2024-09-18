c045fba06fb0d4f3e543f20767e4f26e39565bae
riteshkarwa <riteshkarwa@yahoo.com>
2024-09-17 23:04:37 -0500

    fix code

--- test_google_finance.py
+++ test_google_finance.py
@@ -11,7 +11,9 @@ from selenium.webdriver.common.by import By
 
 @pytest.mark.parametrize("data", [["NFLX","MSFT", "TSLA"]])
 def test_google_finance(data):
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    option = webdriver.ChromeOptions()
+    option.add_argument("start-maximized")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=option)
     driver.get("https://www.google.com/finance/")
     assert "Google Finance - Stock Market Prices, Real-time Quotes & Business News" == driver.title
     css_selector_for_stock_symbol = ("#yDmH0d > c-wiz.zQTmif.SSPGKf.ccEnac > div > div.e1AOyf > div > div > div.fAThCb > "
