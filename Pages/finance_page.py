fce5db4f1765421ae14dd5d34e34e48aa7e13156
riteshkarwa <riteshkarwa@yahoo.com>
2024-09-18 09:43:14 -0500

    final code push

--- Pages/finance_page.py
+++ Pages/finance_page.py
@@ -21,15 +21,18 @@ class FinancePage:
         )
         stock_symbol = self.driver.find_elements(By.CSS_SELECTOR,
                                             css_selector_for_stock_symbol)
+        # Compare and print stock symbol
         print("\n")
         stock_symbol_from_site = []
         for i in stock_symbol:
             stock_symbol_from_site.append(i.text)
-
+        # Print all stock symbols that are on the site  but not in given test data
         for i in stock_symbol_from_site:
             if i not in data:
                 print(i, end=',')
         print("\n")
+
+        # Print all stock symbols that are in given test data  but not on site
         for i in data:
             if i not in stock_symbol_from_site:
                 print(i, end=',')
