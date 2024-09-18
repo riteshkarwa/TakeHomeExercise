a809a71e088f3045d43ef58ff805cd1af8e68305
riteshkarwa <riteshkarwa@yahoo.com>
2024-09-18 00:09:20 -0500

    fix code

--- test_google_finance.py
+++ test_google_finance.py
@@ -10,11 +10,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.parametrize("data", [["NFLX","MSFT", "TSLA"]])
-def test_google_finance(data):
+@pytest.fixture()
+def driver():
     option = Options()
     option.add_argument("--headless")
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=option)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+@pytest.mark.parametrize("data", [["NFLX","MSFT", "TSLA"]])
+def test_google_finance(data,driver):
+
     driver.get("https://www.google.com/finance/")
     assert "Google Finance - Stock Market Prices, Real-time Quotes & Business News" == driver.title
     css_selector_for_stock_symbol = ("#yDmH0d > c-wiz.zQTmif.SSPGKf.ccEnac > div > div.e1AOyf > div > div > div.fAThCb > "
@@ -37,5 +44,3 @@ def test_google_finance(data):
     for i in data:
         if i not in stock_symbol_from_site:
             print(i,end=',')
-    driver.close()
-    driver.quit()
